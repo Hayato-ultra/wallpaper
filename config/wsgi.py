c96cf5038c44b198b8ce671d589d4afa9aab8d24
Hayato-ultra <rohitrtv95@gmail.com>
2026-06-20 20:31:48 +0530

    Run migrate on Vercel cold start instead of build

--- config/wsgi.py
+++ config/wsgi.py
@@ -6,3 +6,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
+
+if os.environ.get('VERCEL'):
+    from django.core.management import call_command
+    call_command('migrate', '--noinput', verbosity=0)
