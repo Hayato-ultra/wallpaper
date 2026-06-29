7cf11f32cdd722d35fc7cd9fe43da652e58e37cb
Hayato-ultra <rohitrtv95@gmail.com>
2026-06-29 12:03:42 +0530

    fix: route /static/ through WSGI for WhiteNoise, use CompressedManifestStaticFilesStorage

--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 WHITENOISE_AUTOREFRESH = False
